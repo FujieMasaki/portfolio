38bfb062165e65aff5c6b11cfa3d6582de3045bc
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2026-09-03 12:09:53 +0900

    feat: ヘッダーを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Career from "@/components/Career";
@@ -9,6 +10,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div className="container">
+      <Header />
       <Hero />
       <About />
       <Career />
